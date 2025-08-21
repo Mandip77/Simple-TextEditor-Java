f9749b8baeb7c8bb2d6b327140b658b7e82d4edc
Mandip Amgain <65205256+Mandip77@users.noreply.github.com>
2025-08-21 09:17:28 -0700

    Add files via upload
    DarkMode feature

---
+++ TextEditor/src/main/java/edu/bhcc/mandip/DarkModeUtil.java
@@ -0,0 +1,17 @@
+package your.package.name;
+
+import java.awt.Color;
+
+public class DarkModeUtil {
+    // Light Mode Colors
+    public static final Color LIGHT_BG = Color.WHITE;
+    public static final Color LIGHT_FG = Color.BLACK;
+    public static final Color LIGHT_MENU_BG = new Color(238, 238, 238);
+    public static final Color LIGHT_MENU_FG = Color.BLACK;
+
+    // Dark Mode Colors
+    public static final Color DARK_BG = new Color(30, 30, 30);
+    public static final Color DARK_FG = new Color(220, 220, 220);
+    public static final Color DARK_MENU_BG = new Color(45, 45, 45);
+    public static final Color DARK_MENU_FG = new Color(220, 220, 220);
+}
